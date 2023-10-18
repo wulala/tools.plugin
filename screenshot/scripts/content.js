6280f7f4a22c3d2b06a84f3d6a8c0ea48e08fa7c
wulala <189267167@qq.com>
2023-10-18 18:14:33 +0800

    feat: ui

--- screenshot/scripts/content.js
+++ screenshot/scripts/content.js
@@ -2,31 +2,51 @@ let captureInfo = {
     base64: [],
 }
 
-// 工具条插入
+let overCover = null // 鼠标移入的覆盖层（用于标记选中的区域）
+let nodeCover = null // 鼠标选中的元素的提示层
+let toPicBtn = null // 生成按钮
+
+let overElem = null // 鼠标移入块元素
+let selectElem = null // 选中元素
+
+// ----------------------------- 工具条插入 start -----------------------------
 let toolbar = document.createElement('five-toolbar')
-toolbar.classList.add(`__five-toolbar`)
-toolbar.innerHTML = `<five-select class="__five-select">
-<svg viewBox="0 0 1024 1024">
-    <path
-        d="M990.691556 731.847111L1024 688.967111 577.550222 342.101333 848.298667 71.352889 0 0.839111l70.513778 848.312889 261.034666-261.048889 355.328 435.057778 42.069334-34.346667-393.329778-481.592889L114.944 727.907556 59.434667 60.273778 727.068444 115.768889 495.630222 347.221333z"
-    ></path>
-</svg>
-</five-select>
-<five-select class="__five-exit">
-<svg viewBox="0 0 1024 1024">
-    <path
-        d="M559.786667 512l314.026666-314.026667c13.653333-13.653333 13.653333-34.133333 0-47.786666-13.653333-13.653333-34.133333-13.653333-47.786666 0L512 464.213333 197.973333 150.186667c-13.653333-13.653333-34.133333-13.653333-47.786666 0-13.653333 13.653333-13.653333 34.133333 0 47.786666l314.026666 314.026667-314.026666 314.026667c-13.653333 13.653333-13.653333 34.133333 0 47.786666 13.653333 13.653333 34.133333 13.653333 47.786666 0l314.026667-314.026666 314.026667 314.026666c13.653333 13.653333 34.133333 13.653333 47.786666 0 13.653333-13.653333 13.653333-34.133333 0-47.786666L559.786667 512z"
-    ></path>
-</svg>
-</five-select>
-<five-cover class="__five-cover"></five-cover>`
+toolbar.innerHTML = `
+<five-tool class="__five-tool">
+    <five-btn class="__five-select">
+    <svg viewBox="0 0 1024 1024">
+        <path
+            d="M990.691556 731.847111L1024 688.967111 577.550222 342.101333 848.298667 71.352889 0 0.839111l70.513778 848.312889 261.034666-261.048889 355.328 435.057778 42.069334-34.346667-393.329778-481.592889L114.944 727.907556 59.434667 60.273778 727.068444 115.768889 495.630222 347.221333z"
+        ></path>
+    </svg>
+    </five-btn>
+    <five-btn class="__five-generate">
+    <svg viewBox="0 0 1024 1024">
+        <path
+            d="M779.7 588.6c-23.2 0-45.3 4.4-65.7 12.5L282.9 108.3c-10.2-11.6-27.9-12.8-39.5-2.6-11.6 10.2-12.8 27.9-2.6 39.5l424.4 485.1c-39.3 32.8-64.3 82.2-64.3 137.2 0 98.6 80.2 178.8 178.8 178.8 98.6 0 178.8-80.2 178.8-178.8s-80.2-178.9-178.8-178.9z m0 301.7c-67.7 0-122.8-55.1-122.8-122.8 0-67.7 55.1-122.8 122.8-122.8 67.7 0 122.8 55.1 122.8 122.8 0 67.7-55.1 122.8-122.8 122.8z"
+        ></path>
+        <path
+            d="M779.7 105.6c-11.6-10.2-29.3-9-39.5 2.6L309 601.1c-20.3-8.1-42.5-12.5-65.7-12.5-98.6 0-178.8 80.2-178.8 178.8s80.2 178.8 178.8 178.8c98.6 0 178.8-80.2 178.8-178.8 0-55.1-25-104.4-64.3-137.2l424.4-485.1c10.3-11.6 9.1-29.3-2.5-39.5zM243.4 890.3c-67.7 0-122.8-55.1-122.8-122.8 0-67.7 55.1-122.8 122.8-122.8 67.7 0 122.8 55.1 122.8 122.8 0 67.7-55.1 122.8-122.8 122.8z"
+        ></path>
+    </svg>
+    </five-btn>
+    <five-btn class="__five-exit">
+    <svg viewBox="0 0 1024 1024">
+        <path
+            d="M559.786667 512l314.026666-314.026667c13.653333-13.653333 13.653333-34.133333 0-47.786666-13.653333-13.653333-34.133333-13.653333-47.786666 0L512 464.213333 197.973333 150.186667c-13.653333-13.653333-34.133333-13.653333-47.786666 0-13.653333 13.653333-13.653333 34.133333 0 47.786666l314.026666 314.026667-314.026666 314.026667c-13.653333 13.653333-13.653333 34.133333 0 47.786666 13.653333 13.653333 34.133333 13.653333 47.786666 0l314.026667-314.026666 314.026667 314.026666c13.653333 13.653333 34.133333 13.653333 47.786666 0 13.653333-13.653333 13.653333-34.133333 0-47.786666L559.786667 512z"
+        ></path>
+    </svg>
+    </five-btn>
+</five-tool>
+<five-cover class="__five-cover"></five-cover>
+<five-node class="__five-node"></five-node>
+`
 document.body.appendChild(toolbar)
 
-
-// 鼠标移入的覆盖层（用于标记选中的区域）
-let cover = document.querySelector('.__five-cover')
-
+// 初始化截图工具
 document.querySelector('.__five-select').addEventListener('click', (e) => {
+    if (e.target.classList.contains('__five-on')) return console.log('已经启用选择了')
+
     e.target.classList.add('__five-on')
 
     // 移除所有事件
@@ -34,99 +54,91 @@ document.querySelector('.__five-select').addEventListener('click', (e) => {
     let clonedElement = body.cloneNode(true)
     body.parentNode.replaceChild(clonedElement, body)
 
-    // 重新获取元素
-    cover = document.querySelector('.__five-cover')
+    // 移除所有hover效果
+    removeHover()
+
+    // 获取元素
+    overCover = document.querySelector('.__five-cover')
+    nodeCover = document.querySelector('.__five-node')
+    toPicBtn = document.querySelector('.__five-generate')
 
     //  重新添加事件
-    document.querySelector('.__five-exit').addEventListener('click', (e) => location.reload())
+    document.querySelector('.__five-exit').addEventListener('click', (e) => {
+        location.reload()
+    })
 
+    toPicBtn.addEventListener('click', (e) => {
+        if (!selectElem) return
+
+        start()
+    })
 
     // 添加选择事件
     document.body.addEventListener('mouseover', over, false)
-    // document.body.addEventListener('mouseout', out, false)
     document.body.addEventListener('click', click, false)
-
 })
 
-// 刷新页面，因为事件都被移除了
-document.querySelector('.__five-exit').addEventListener('click', (e) => location.reload())
-
-
-
-
+// 退出截图工具
+document.querySelector('.__five-exit').addEventListener('click', (e) => {
+    if (document.querySelector('.__five-select').classList.contains('__five-on')) {
+        return location.reload()
+    }
+    document.body.removeChild(toolbar)
+})
 
 /**
- * ---------------------------选择dom元素开始-------------------------------
+ * 鼠标移入元素
+ * @param {*} e
+ * @returns
  */
-
-let overElem = null // 鼠标移入元素
-let selectElem = null // 选中元素
-
-
 function over(e) {
-
-
-    if (e.target.classList.value.indexOf('__five') != -1) return console.log('mouseover 我是插件元素')
+    if (e.target.classList.value.indexOf('__five') != -1) {
+        overCover.classList.remove(`__five-show`)
+        return console.log('插件元素不可被选择')
+    }
 
     overElem = findClosestBlockElement(e.target)
     if (!overElem) return console.log('没有找到block元素')
-    if (selectElem) return console.log('已经有选中元素了，因为要对选中元素编辑')
 
     let rect = overElem.getBoundingClientRect()
-    cover.style.transform = `translate(${rect.x}px, ${rect.y}px)`
-    cover.style.width = `${rect.width}px`
-    cover.style.height = `${rect.height}px`
-
-    cover.classList.add(`__five-show`)
-}
-function out(e) {
-    if (e.target.classList.value.indexOf('__five') != -1) return console.log('mouseout, 我是插件元素')
-    if (selectElem == overElem) return console.log('当前已选中，要保留选中状态哦')
-    cover.classList.remove(`__five-show`)
-}
-function click(e) {
-    e.preventDefault()
-
-    // 如果已选中，再次点击
-    if (selectElem == overElem) {
-        selectElem = null
-        cover.classList.remove(`__five-show`)
-        return
-    }
-    // 没过没选中
-    if (selectElem) {
-        cover.classList.remove(`__five-show`)
-    }
-    selectElem = overElem
-    cover.classList.add(`__five-show`)
-}
-// 判断元素本身是否为块级元素
-function isBlockElement(element) {
-    const computedStyle = window.getComputedStyle(element)
-    const displayValue = computedStyle.getPropertyValue('display')
-    return (
-        displayValue === 'block' ||
-        displayValue === 'inline-block' ||
-        displayValue === 'list-item' ||
-        displayValue === 'table' ||
-        displayValue === 'table-cell' ||
-        displayValue === 'flex' ||
-        displayValue === 'inline-flex' ||
-        displayValue === 'grid' ||
-        displayValue === 'inline-grid'
-    )
-}
-// 查找具有块级显示属性的最近父元素,(找块元素)
-function findClosestBlockElement(element) {
-    if (isBlockElement(element)) return element
-    if (element.parentElement) return findClosestBlockElement(element.parentElement)
-    return null
+    overCover.style.transform = `translate(${rect.x}px, ${rect.y}px)`
+    overCover.style.width = `${rect.width}px`
+    overCover.style.height = `${rect.height}px`
+    overCover.classList.add(`__five-show`)
 }
 
 /**
- * ---------------------------选择dom元素完成-------------------------------
+ * 选中元素
+ * @param {*} e
+ * @returns
  */
+function click(e) {
+    e.preventDefault()
 
+    if (e.target.classList.value.indexOf('__five') != -1) return console.log('插件元素不可被选择')
+    if (!overElem) return console.log('没有找到block元素')
+
+    if (selectElem == overElem) {
+        selectElem = null
+        toPicBtn.classList.remove('__five-can')
+        nodeCover.classList.remove(`__five-show`)
+        return
+    }
+
+    selectElem = overElem
+    toPicBtn.classList.add('__five-can')
+
+    let rect = overElem.getBoundingClientRect()
+    nodeCover.style.transform = `translate(${rect.x}px, ${rect.y}px)`
+    nodeCover.style.width = `${rect.width}px`
+    nodeCover.style.height = `${rect.height}px`
+    nodeCover.classList.add(`__five-show`)
+}
+
+/**
+ * 开始截图
+ * @param {*} elem
+ */
 function start(elem) {
     // reset
     captureInfo = { base64: [] }
@@ -166,10 +178,6 @@ function start(elem) {
  * 处理页面
  */
 function handlerDom() {
-    // 隐藏 __hover
-    // selectElem.classList.remove('__hover')
-    // blockElem.classList.remove('__hover')
-
     selectElem.removeChild(cover)
     blockElem.removeChild(cover)
 
@@ -256,7 +264,48 @@ async function complete() {
     document.querySelector('body').appendChild(canvas)
 }
 
-function scroll(e) {
-    e.stopPropagation()
-    e.preventDefault()
+function out(e) {
+    if (e.target.classList.value.indexOf('__five') != -1) return console.log('mouseout, 我是插件元素')
+    if (selectElem == overElem) return console.log('当前已选中，要保留选中状态哦')
+    cover.classList.remove(`__five-show`)
 }
+
+// ---------------------------------------辅助函数 start-------------------------------------------------------------
+
+// 判断元素本身是否为块级元素
+function isBlockElement(element) {
+    const computedStyle = window.getComputedStyle(element)
+    const displayValue = computedStyle.getPropertyValue('display')
+    return (
+        displayValue === 'block' ||
+        displayValue === 'inline-block' ||
+        displayValue === 'list-item' ||
+        displayValue === 'table' ||
+        displayValue === 'table-cell' ||
+        displayValue === 'flex' ||
+        displayValue === 'inline-flex' ||
+        displayValue === 'grid' ||
+        displayValue === 'inline-grid'
+    )
+}
+// 查找具有块级显示属性的最近父元素,(找块元素)
+function findClosestBlockElement(element) {
+    if (isBlockElement(element)) return element
+    if (element.parentElement) return findClosestBlockElement(element.parentElement)
+    return null
+}
+
+function removeHover() {
+    // 遍历每个元素，并移除其hover伪类样式
+    document.querySelectorAll('*').forEach((element) => {
+        const computedStyle = window.getComputedStyle(element)
+        const originalStyle = computedStyle.getPropertyValue('style')
+        const hoverStyle = computedStyle.getPropertyValue('hover')
+
+        if (hoverStyle !== 'none') {
+            element.style.setProperty('style', originalStyle)
+        }
+    })
+}
+
+// ---------------------------------------辅助函数 end------------------------------------------------------
